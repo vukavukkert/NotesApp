692cbac3d22b405ca393f8f842765064e9901b6d
user <user@VUKKERT>
2024-03-03 12:39:01 +0700

    js storage

--- script.js
+++ script.js
@@ -1,29 +1,72 @@
- 
-  function createTask(){
-    const content = document.getElementById('content').value;
-    if(content){
-      const task = document.createElement('li');
-      task.className = 'flex-row-container element-generic-style task awaiting-task';
-      task.innerHTML = `
+let taskList = [{content: "1", isCompleted: false}];
+
+loadTasks();
+
+function loadTasks(){
+    taskList = (JSON.parse(Cookies.get("taskList")))
+    task.forEach(element => {
+        drawTask(element.content, element.isCompleted);
+    });
+}
+
+function createTask(){ // лежит на кнопке
+    const content = document.getElementById("content").value;
+    taskList.push({ content: content, isCompleted: false });
+    saveList();
+    drawTask(content, false);
+}
+
+function drawTask(content, isCompleted) { // выводит на экран
+       if (content) {
+        const task = document.createElement("li");
+        task.className =
+            "flex-row-container element-generic-style task awaiting-task";
+        task.innerHTML = `
           <button onclick="completeTask(this)"></button>
           <p id="taskContent">${content}</p>
           <button onclick="deleteTask(this)">X</button>
       `;
-      document.getElementById('tasks').appendChild(task);
-      document.getElementById('content').value = "";
+      if(isCompleted){
+        task.className = "flex-row-container element-generic-style task completed-task";
+      }
+        document.getElementById("tasks").appendChild(task);
+        document.getElementById("content").value = "";
     }
-    }
-    function completeTask(button){
-      const task = button.parentNode;
-      task.className = 'flex-row-container element-generic-style task completed-task';
-    }
+}
+function completeTask(button) {
+    const task = button.parentNode;
+    task.className =
+        "flex-row-container element-generic-style task completed-task";
+        taskList.forEach(element => {
+            if(element.content == taskContent(task)){
+                element.isCompleted = true;
+            }
+        });
+        saveList();
+ }
+function deleteTask(button) {
+    const task = button.parentNode;
+    taskList.forEach(element => {
+        if (element.content == taskContent(task)){
+            console.log(taskContent(task));
+            taskList.splice(taskList.indexOf(element), 1);
+         }
+    });
+    saveList();
+    task.remove();
+}
 
-    function deleteTask(button){
-      const task = button.parentNode;
-      task.remove();
-    }
+function clearAll() {
+    tasks = document.getElementById("tasks");
+    tasks.innerHTML = "";
+    taskList.clearAll;
+    saveList();
+}
 
-    function clearAll(){
-      tasks = document.getElementById('tasks');
-      tasks.innerHTML = '';
-    }
+function taskContent(task){
+    let content = task.querySelector('#taskContent').innerHTML;
+    return content;
+}
+function saveList(){
+    Cookies.set("taskList", JSON.stringify(taskList));
+}
